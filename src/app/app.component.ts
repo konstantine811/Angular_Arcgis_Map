2046f93c11496b3b956eb3e32c43339dc42014f5
Константин Абрамкин <konstantinabramkin@MacBook-Pro-Konstantin.local>
2021-09-07 13:10:54 +0300

    initial view arcgis map :

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+// arcgis
+import Map from '@arcgis/core/Map';
+import SceneView from '@arcgis/core/views/SceneView';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-map';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('mapViewNode') private mapViewEl!: ElementRef;
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    const map = new Map({
+      basemap: 'topo-vector',
+      ground: 'world-elevation',
+    });
+    new SceneView({
+      container: this.mapViewEl.nativeElement,
+      map,
+      scale: 50000000,
+      center: [-101.17, 21.78],
+    });
+  }
 }
